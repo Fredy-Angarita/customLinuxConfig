3573db8a1c5e1cc94e15e30dd515314cfbf15002
Fredy Angarita <100323436+Fredy-Angarita@users.noreply.github.com>
2025-05-12 14:29:16 -0500

    Update headphones.sh

--- headphones.sh
+++ headphones.sh
@@ -3,4 +3,5 @@
 SINK="alsa_output.pci-0000_00_1f.3.analog-stereo"
 PORT="analog-output-headphones"
 pactl set-sink-port $SINK $PORT
+echo "Switched to headphones"
 echo "$SINK $PORT" > ~/.config/customBash/last_port.conf    
