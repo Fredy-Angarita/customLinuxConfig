bfd30116cf048547e3ed68a528175a09ca18d464
Fredy Angarita <100323436+Fredy-Angarita@users.noreply.github.com>
2025-05-12 14:29:51 -0500

    Update lineout.sh

--- lineout.sh
+++ lineout.sh
@@ -3,4 +3,5 @@
 SINK="alsa_output.pci-0000_00_1f.3.analog-stereo"
 PORT="analog-output-lineout"
 pactl set-sink-port $SINK $PORT
+echo "Switched to Speakers"
 echo "$SINK $PORT" > ~/.config/customBash/last_port.conf
